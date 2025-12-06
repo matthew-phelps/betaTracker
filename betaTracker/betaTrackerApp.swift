eab04c41469f49695d4a070bdb778fec789452b0
MEWP (Matthew Phelps) <MEWP+nngithub@novonordisk.com>
2025-12-06 08:55:23 +0100

    Initial Commit

---
+++ betaTracker/betaTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  betaTrackerApp.swift
+//  betaTracker
+//
+//  Created by MEWP (Matthew Phelps) on 06/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct betaTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
